7da844ba1090c05d1b56ea3acb27fc448e3a81ce
Maciej Jur <mj@kamoshi.org>
2022-01-13 22:07:47 +0100

    Solve chapter 1

---
+++ chapter01/main.hs
@@ -0,0 +1,16 @@
+-- 3
+product_ [] = 0
+product_ [x] = x
+product_ (x:xs) = x * product xs
+
+-- 4
+qsort [] = []
+qsort (x:xs) = qsort bigger ++ [x] ++ qsort smaller
+    where
+        smaller = [a | a <- xs, a < x]
+        bigger = [b | b <- xs, b >= x]
+
+main = do
+    print "Hello World!"
+    print $ product_ [2, 3, 4]
+    print $ qsort [5, 2, 4, 1, 9, 4, 7]
