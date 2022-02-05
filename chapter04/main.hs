98621076fc60785586ecf5ccf18825d1564d0ebc
Maciej Jur <mj@kamoshi.org>
2022-02-05 16:13:00 +0100

    Tautology thing

--- chapter04/main.hs
+++ chapter04/main.hs
@@ -4,7 +4,7 @@ halve xs = (take (length xs `div` 2) xs, drop (length xs `div` 2) xs)
 -- 2
 third' xs = head $ tail $ tail xs
 third'' xs = xs !! 2
-third''' (_:_:x:_) = x
+--third''' (_:_:x:_) = x
 
 -- 3
 safetail' xs = if null xs then [] else tail xs
@@ -14,13 +14,26 @@ safetail'' xs
 safetail''' [] = []
 safetail''' (_:xs) = xs
 
+-- 4
+(|||) True _ = True
+(|||) False y = y
+
+(||||) x y = if x then x else y
+
+-- 5
+(&&&) x y = if x then if y then True else False else False
+
+-- 6
+(&&&&) True b = if b then True else False 
+(&&&&) _ _ = False
+
 
 main = do
       let xs = [1..6]
       print $ halve xs
       print $ third' xs
       print $ third'' xs
-      print $ third''' xs
+      --print $ third''' xs
       print $ safetail' xs
       print $ safetail'' xs
       print $ safetail''' xs
